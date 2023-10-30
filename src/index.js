9ec5dfebe339acc0548b82a4b6eb8d2137c910e7
valentinacamacho <valentinacamacho0206@gmail.com>
2023-10-29 20:11:02 -0500

    Added backend folder structure and added new dependencies.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+//routes
+app.get('/', (req, res) =>
+res.send("Welcome to my Api"));
+
+//mongodb connection
+mongoose.connect();
+
 app.listen(port, () => console.log('Server  listening on port', port));
+
